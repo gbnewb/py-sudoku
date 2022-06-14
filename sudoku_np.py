39dece12b4ed42826f3e8947061377f9c8fdef57
gbnewb local <88551948+gbnewb@users.noreply.github.com>
2022-06-14 20:53:00 +0800

    Fix format

--- sudoku_np.py
+++ sudoku_np.py
@@ -25,70 +25,70 @@ def check_print(board):
     
           
 def point_validator(board, point):
-        """check if the given point is valid in the Sudoku object
+    """check if the given point is valid in the Sudoku object
 
-        Args:
-            point (tuple): a (x,y) coordinate
+    Args:
+        point (tuple): a (x,y) coordinate
 
-        Returns:
-            True or False
-        """
-        y, x = point
-        value = board[y, x]
-        if value:
-            board[y, x] = 0
-            row = board[y]
-            column = board[:, x]
-            subgrid = [board[i, j] for i in range(y//3*3, y//3*3+3) for j in range(x//3*3, x//3*3+3)]
-            if (value in row) or (value in column) or (value in subgrid):
-                board[y, x] = value
-                return False
-        board[y, x] = value
-        return True
+    Returns:
+        True or False
+    """
+    y, x = point
+    value = board[y, x]
+    if value:
+        board[y, x] = 0
+        row = board[y]
+        column = board[:, x]
+        subgrid = [board[i, j] for i in range(y//3*3, y//3*3+3) for j in range(x//3*3, x//3*3+3)]
+        if (value in row) or (value in column) or (value in subgrid):
+            board[y, x] = value
+            return False
+    board[y, x] = value
+    return True
 
 
 def validator(board):
-        valid_items = np.arange(10)
-        if len(board) != 9:
-            return False
-        else:
-            for x_axis in board:
-                if len(x_axis) != 9:
+    valid_items = np.arange(10)
+    if len(board) != 9:
+        return False
+    else:
+        for x_axis in board:
+            if len(x_axis) != 9:
+                return False
+            for item in x_axis:
+                if item not in valid_items:
                     return False
-                for item in x_axis:
-                    if item not in valid_items:
-                        return False
-        for y in range(9):
-            for x in range(9):
-                if not point_validator(board, (y, x)):
-                    return False
-        return True
+    for y in range(9):
+        for x in range(9):
+            if not point_validator(board, (y, x)):
+                return False
+    return True
     
 
 def attempt(board, algorithm='BackTracking'):
-        unfilled = [(i, j) for i in range(9) for j in range(9) if board[i, j] == 0]
-        
-        if algorithm == 'BackTracking':
-            back_tracking_count = 0
-            trash = {}
-            trash_full = {}
-            i = 0
-            y, x = unfilled[i]
-            [trash.setdefault(point, set()) for point in unfilled]
-            [trash_full.setdefault(point, set(range(1, 10))) for point in unfilled]
-                
-            while trash != trash_full:
-                back_tracking_count += 1
-                board[y, x] += 1
-                trash[unfilled[i]].add(board[y, x])
-                if point_validator(board, (y, x)):
-                    if i == len(unfilled) - 1:
-                        return board, back_tracking_count
-                    i += 1
+    unfilled = [(i, j) for i in range(9) for j in range(9) if board[i, j] == 0]
+    
+    if algorithm == 'BackTracking':
+        back_tracking_count = 0
+        trash = {}
+        trash_full = {}
+        i = 0
+        y, x = unfilled[i]
+        [trash.setdefault(point, set()) for point in unfilled]
+        [trash_full.setdefault(point, set(range(1, 10))) for point in unfilled]
+            
+        while trash != trash_full:
+            back_tracking_count += 1
+            board[y, x] += 1
+            trash[unfilled[i]].add(board[y, x])
+            if point_validator(board, (y, x)):
+                if i == len(unfilled) - 1:
+                    return board, back_tracking_count
+                i += 1
+                y, x = unfilled[i]
+            else:
+                while board[y, x] == 9:
+                    board[y, x] = 0
+                    i -= 1
                     y, x = unfilled[i]
-                else:
-                    while board[y, x] == 9:
-                        board[y, x] = 0
-                        i -= 1
-                        y, x = unfilled[i]
-            return board
+    return board
